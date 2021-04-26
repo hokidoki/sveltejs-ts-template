2269fe1897aae22be7003c6dc39d74a76d7c9b8b
이종호 <substantiation@naver.com>
2021-04-26 22:36:11 +0900

    init

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  let hello = "world";
+</script>
+
+<div>
+  <h1>
+    hello {hello}
+  </h1>
+</div>
+
+<style lang="scss">
+  div {
+    h1 {
+      color: blue;
+    }
+  }
+</style>
